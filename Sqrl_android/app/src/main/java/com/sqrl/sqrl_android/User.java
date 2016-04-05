fb9295e56f787d4ebba03adf159a670e4e544094
Shawn <ssburns@gmail.com>
2016-04-05 16:10:27 -0700

    Recovered working project

---
+++ Sqrl_android/app/src/main/java/com/sqrl/sqrl_android/User.java
@@ -0,0 +1,31 @@
+package com.sqrl.sqrl_android;
+
+
+public class User {
+
+    static String firstName;
+    static String lastName;
+    static String password;
+
+
+
+    public  void setFirstName(String fname){
+        this.firstName = fname;
+    }
+    public  void setLastName(String lname){
+        this.lastName = lname;
+    }
+    public void setPassword(String pword){
+        this.password = pword;
+    }
+    public static String getFirstName(){
+        return firstName;
+    }
+    public static String getLastName(){
+        return  lastName;
+    }
+    public static String getPassword(){return password;}
+
+}
+
+
