ce9d734587bd7cec9fb1d74f235585e3b91fd543
sb <ssburns@gmail.com>
2016-04-16 22:10:42 -0700

    testing volley

--- Sqrl_android/app/src/main/java/com/sqrl/sqrl_android/MainActivity.java
+++ Sqrl_android/app/src/main/java/com/sqrl/sqrl_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.sqrl.sqrl_android;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -47,5 +48,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void httpSend(View vew) {
+        //Test the Volley HTTP library to send data
+        Log.d("http test", "http test button pressed");
+
+
     }
 }
