cc325a50f32855a693d827ba2d15925577ada32e
Lori Overholtzer <PCPrincess@mail.fresnostate.edu>
2016-05-09 13:14:57 -0700

    A few testing updates.

--- Sqrl_android/app/src/test/java/com/sqrl/sqrl_android/activities/MainActivityUnitTest.java
+++ Sqrl_android/app/src/test/java/com/sqrl/sqrl_android/activities/MainActivityUnitTest.java
@@ -1,9 +1,20 @@
 package com.sqrl.sqrl_android.activities;
 
 import android.app.Application;
+import android.content.Context;
 import android.test.ApplicationTestCase;
+import android.view.View;
+import android.view.ViewStub;
+
+import com.android.volley.RequestTest;
+import com.android.volley.toolbox.Volley;
+import com.sqrl.sqrl_android.R;
+import com.sqrl.sqrl_android.data.ClientInfoParam;
+import com.sqrl.sqrl_android.helpers.AuthenticationPostBody;
+
 import org.junit.Test;
 import java.util.regex.Pattern;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -12,6 +23,13 @@ import static org.junit.Assert.assertTrue;
  */
 public class MainActivityUnitTest {
     public MainActivityUnitTest() {
+        MainActivity activity = new MainActivity();
+        // test httpSend
+        Context testContext;
+        ClientInfoParam testClient = new ClientInfoParam("1", "cmd", "2468", "1357", " ", " ");
+        AuthenticationPostBody testAuthData = new AuthenticationPostBody(testClient, "parserver", "parids", "parpids", "parurs");
+        View testView = null;
+        activity.httpSend(testView);
 
     }
 }
